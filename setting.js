7bac9dc02e5c8e39aee93fc250568217f3e45d4b
Kaizatz <kaizatz01@gmail.com>
2025-11-26 16:26:42 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,10 +1,10 @@
 //=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
-  SESSION_ID: '', // add your session ID
+  BOT_NAME: 'KAIZA TZ', // ur bot name
+  OWNER_NAME: 'KAIZA TZ', //your name
+  OWNER_NUMBER: '255768843060', // add your phone number
+  SESSION_ID: 'https://whatsapp.com/channel/0029Vb7Av75JENyB7jkHaY0G', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
